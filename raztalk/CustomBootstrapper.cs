87a193ff6dec41bb4893d24d56b064c2dd1b7f53
Gábor Görzsöny <gabor@gorzsony.com>
2018-08-10 23:20:09 +0200

    Initial commit

---
+++ raztalk/CustomBootstrapper.cs
@@ -0,0 +1,83 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Conventions;
+using Nancy.Diagnostics;
+using Nancy.Security;
+using Nancy.Session;
+using Nancy.TinyIoc;
+using Nancy.ViewEngines;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace raztalk
+{
+    public class CustomBootstrapper : DefaultNancyBootstrapper
+    {
+        //private byte[] favicon;
+
+        //protected override byte[] FavIcon
+        //{
+        //    get { return this.favicon ?? (this.favicon = LoadFavIcon()); }
+        //}
+
+        //private byte[] LoadFavIcon()
+        //{
+        //    using (var resourceStream = GetType().Assembly.GetManifestResourceStream("raztalk.Content.favicon.png"))
+        //    {
+        //        var memoryStream = new MemoryStream();
+        //        resourceStream.CopyTo(memoryStream);
+        //        return memoryStream.GetBuffer();
+        //    }
+        //}
+
+        protected override NancyInternalConfiguration InternalConfiguration
+        {
+            get
+            {
+                return NancyInternalConfiguration.WithOverrides(OnConfigurationBuilder);
+            }
+        }
+
+        void OnConfigurationBuilder(NancyInternalConfiguration x)
+        {
+            x.ViewLocationProvider = typeof(ResourceViewLocationProvider);
+        }
+
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string[] assemblyNames = assembly.GetManifestResourceNames();
+
+            base.ConfigureApplicationContainer(container);
+            ResourceViewLocationProvider.RootNamespaces.Add(GetType().Assembly, "raztalk.Views");
+        }
+
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            nancyConventions.StaticContentsConventions.Add(StaticResourceConventionBuilder.AddDirectory("/", assembly, "raztalk.Content"));
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            CookieBasedSessions.Enable(pipelines);
+            SSLProxy.RewriteSchemeUsingForwardedHeaders(pipelines);
+        }
+
+        protected override IEnumerable<ModuleRegistration> Modules
+        {
+            get
+            {
+                return AppDomainAssemblyTypeScanner
+                        .TypesOf<INancyModule>(ScanMode.All)
+                        .NotOfType<DiagnosticModule>()
+                        .Select(t => new ModuleRegistration(t))
+                        .ToArray();
+            }
+        }
+    }
+}
---
+++ raztalk/StaticResourceConventionBuilder.cs
@@ -0,0 +1,62 @@
+﻿/*
+Copyright (C) Gábor "Razzie" Görzsöny
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
+*/
+
+using Nancy;
+using Nancy.Responses;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace raztalk
+{
+    public static class StaticResourceConventionBuilder
+    {
+        public static Func<NancyContext, string, Response> AddDirectory(string requestedPath, Assembly assembly, string namespacePrefix)
+        {
+            return (context, s) =>
+            {
+                var path = context.Request.Path;
+
+                if (!path.StartsWith(requestedPath))
+                {
+                    return null;
+                }
+
+                string resourcePath;
+                string name;
+
+                var adjustedPath = path.Substring(requestedPath.Length + 1);
+
+                if (adjustedPath.IndexOf('/') >= 0)
+                {
+                    name = Path.GetFileName(adjustedPath);
+                    resourcePath = namespacePrefix + "." + adjustedPath
+                        .Substring(0, adjustedPath.Length - name.Length - 1)
+                        .Replace('/', '.');
+                }
+                else
+                {
+                    name = adjustedPath;
+                    resourcePath = namespacePrefix;
+                }
+
+                return new EmbeddedFileResponse(assembly, resourcePath, name);
+            };
+        }
+    }
+}
