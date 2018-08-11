78b10e49848daf8da520af5e2f69dd43115b1dc9
Razzie <hello@razzie.me>
2018-08-12 01:10:00 +0200

    Fixed a few bugs

--- raztalk/Content/raztalk.js
+++ raztalk/Content/raztalk.js
@@ -1,4 +1,19 @@
 ﻿$(document).ready(function () {
+    var last_user;
+    var token = $("body").data("token");
+    var channel = $.connection.channelHub;
+
+    channel.client.send = function (user, message, timestamp) {
+
+    };
+    channel.client.sendInfo = function (info) {
+
+    };
+
+    $.connection.hub.start().done(function () {
+        channel.server.login(token);
+    });
+
     window.sr = ScrollReveal({ reset: true });
     sr.reveal('.reveal');
 });
