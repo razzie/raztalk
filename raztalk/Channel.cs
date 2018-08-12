ad063f3955f496e54cd0bc4463e58ffe7ad6d929
Razzie <hello@razzie.me>
2018-08-12 19:03:07 +0200

    Fixed disconnect handling + new favicon

--- raztalk/Channel.cs
+++ raztalk/Channel.cs
@@ -55,6 +55,7 @@ namespace raztalk
             if (Password.Equals(password))
             {
                 m_users.Add(user);
+                AddMessage(new Message(User.System, user.Name + " connected"));
                 return true;
             }
 
@@ -64,6 +65,7 @@ namespace raztalk
         public void Logout(User user)
         {
             m_users.Remove(user);
+            AddMessage(new Message(User.System, user.Name + " disconnected"));
 
             if (m_users.Count == 0)
                 m_channels.Remove(Name);
