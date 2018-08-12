fe9a5d751eb4b235805d2d6dcf7e5ebeee4f0ad2
Razzie <hello@razzie.me>
2018-08-12 21:14:17 +0200

    raztalk.client: open urls in default browser

--- raztalk.client/MainForm.cs
+++ raztalk.client/MainForm.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,6 +22,51 @@ namespace raztalk.client
         private void MainForm_Load(object sender, EventArgs e)
         {
             web.ScriptErrorsSuppressed = true;
+            SHDocVw.WebBrowser wbCOMmain = (SHDocVw.WebBrowser)web.ActiveXInstance;
+            wbCOMmain.NewWindow3 += wbCOMmain_NewWindow3;
+        }
+
+        void wbCOMmain_NewWindow3(ref object ppDisp,
+                          ref bool Cancel,
+                          uint dwFlags,
+                          string bstrUrlContext,
+                          string bstrUrl)
+        {
+            // bstrUrl is the url being navigated to
+            Cancel = true; // stop the navigation
+
+            // Do whatever else you want to do with that URL
+            // open in the same browser or new browser, etc.
+            OpenUrl(bstrUrl);
+        }
+
+        static private void OpenUrl(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
     }
 }
