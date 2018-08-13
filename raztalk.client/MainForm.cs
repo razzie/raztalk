cc6e39cfa9f2110ffa01744a791d00d07516e01a
Razzie <hello@razzie.me>
2018-08-14 00:08:42 +0200

    raztalk.client reacts to title changes and flashes

--- raztalk.client/MainForm.cs
+++ raztalk.client/MainForm.cs
@@ -21,9 +21,25 @@ namespace raztalk.client
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            GotFocus += MainForm_GotFocus;
+
             web.ScriptErrorsSuppressed = true;
+
+            web.DocumentTitleChanged += Web_DocumentTitleChanged;
+
             SHDocVw.WebBrowser wbCOMmain = (SHDocVw.WebBrowser)web.ActiveXInstance;
             wbCOMmain.NewWindow3 += wbCOMmain_NewWindow3;
+        }
+
+        private void MainForm_GotFocus(object sender, EventArgs e)
+        {
+            web.Document.Window.Focus();
+        }
+
+        private void Web_DocumentTitleChanged(object sender, EventArgs e)
+        {
+            this.Text = web.DocumentTitle;
+            FlashWindowEx(this);
         }
 
         void wbCOMmain_NewWindow3(ref object ppDisp,
@@ -67,6 +83,43 @@ namespace raztalk.client
                     throw;
                 }
             }
+        }
+
+        // To support flashing.
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool FlashWindowEx(ref FLASHWINFO pwfi);
+
+        //Flash both the window caption and taskbar button.
+        //This is equivalent to setting the FLASHW_CAPTION | FLASHW_TRAY flags. 
+        public const UInt32 FLASHW_ALL = 3;
+
+        // Flash continuously until the window comes to the foreground. 
+        public const UInt32 FLASHW_TIMERNOFG = 12;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct FLASHWINFO
+        {
+            public UInt32 cbSize;
+            public IntPtr hwnd;
+            public UInt32 dwFlags;
+            public UInt32 uCount;
+            public UInt32 dwTimeout;
+        }
+
+        // Do the flashing - this does not involve a raincoat.
+        public static bool FlashWindowEx(Form form)
+        {
+            IntPtr hWnd = form.Handle;
+            FLASHWINFO fInfo = new FLASHWINFO();
+
+            fInfo.cbSize = Convert.ToUInt32(Marshal.SizeOf(fInfo));
+            fInfo.hwnd = hWnd;
+            fInfo.dwFlags = FLASHW_ALL | FLASHW_TIMERNOFG;
+            fInfo.uCount = UInt32.MaxValue;
+            fInfo.dwTimeout = 0;
+
+            return FlashWindowEx(ref fInfo);
         }
     }
 }
