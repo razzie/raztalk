f4fa234793500e681280d46c3027bc870bc4ef98
Gábor Görzsöny <gabor@gorzsony.com>
2018-08-26 14:12:02 +0200

    Added raztalk.bot project

---
+++ raztalk.bot/Bot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace raztalk.bot
+{
+    public interface IBot : IDisposable
+    {
+        string Name { get; }
+        event EventHandler<string> NewMessage;
+        void ConsumeMessage(string user, string message, DateTime timestamp);
+    }
+
+    public abstract class Bot : IBot
+    {
+        public abstract string Name { get; }
+        public abstract event EventHandler<string> NewMessage;
+        public abstract void ConsumeMessage(string user, string message, DateTime timestamp);
+        public abstract void Dispose();
+    }
+}
