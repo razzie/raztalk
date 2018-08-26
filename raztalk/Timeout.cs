84449a750ef523d20dbcc3c41a183a8430fa1142
Razzie <hello@razzie.me>
2018-08-26 14:32:27 +0200

    Added Timeout class

---
+++ raztalk/Timeout.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Timers;
+
+namespace raztalk
+{
+    public class Timeout
+    {
+        private Timer m_timer;
+
+        public event ElapsedEventHandler Expired;
+
+        private void FireExpiredEvent(object sender, ElapsedEventArgs e)
+        {
+            Stop();
+            Expired?.Invoke(this, e);
+        }
+
+        public void Start(TimeSpan timeout)
+        {
+            Stop();
+
+            m_timer = new Timer(timeout.TotalMilliseconds + 1); // wait for max N seconds until signalR is connected
+            m_timer.Elapsed += FireExpiredEvent;
+            m_timer.AutoReset = false;
+            m_timer.Enabled = true;
+        }
+        
+        public void Stop()
+        {
+            if (m_timer != null)
+            {
+                m_timer.Elapsed -= FireExpiredEvent;
+                m_timer.Dispose();
+                m_timer = null;
+            }
+        }
+    }
+}
